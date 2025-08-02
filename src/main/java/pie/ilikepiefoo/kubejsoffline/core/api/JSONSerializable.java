bb676e02d7a55c4e9df815357897a49f992f4940
Hunter19823 <hspragg@asu.edu>
2025-08-02 15:28:33 -0700

    Added some safety methods to JSONSerializable to safely add to JSON

--- src/main/java/pie/ilikepiefoo/kubejsoffline/core/api/JSONSerializable.java
+++ src/main/java/pie/ilikepiefoo/kubejsoffline/core/api/JSONSerializable.java
@@ -2,10 +2,13 @@ package pie.ilikepiefoo.kubejsoffline.core.api;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public interface JSONSerializable {
     Logger LOG = LogManager.getLogger();
@@ -23,4 +26,90 @@ public interface JSONSerializable {
     }
 
     JsonElement toJSON();
+
+
+    public default <S extends JSONSerializable> void addTo(JsonElement jsonElement, String key, Supplier<S> supplier) {
+        Consumer<JsonElement> collectionAdder = jsonElement instanceof JsonObject jsonObject ?
+                (element) -> jsonObject.add(key, element) :
+                jsonElement instanceof JsonArray jsonArray ?
+                jsonArray::add :
+                null;
+
+        if (collectionAdder == null) {
+            return;
+        }
+        if (supplier == null) {
+            // If the supplier is null, we cannot add anything
+            return;
+        }
+        try {
+            JSONSerializable jsonSerializable = supplier.get();
+            var jsonValue = jsonSerializable != null ? jsonSerializable.toJSON() : null;
+            if (jsonSerializable != null) {
+                collectionAdder.accept(jsonValue);
+            }
+        } catch (final Throwable e) {
+            LOG.warn("Failed to add JSONSerializable to JSONElement", e);
+        }
+    }
+
+    public default <S extends JSONSerializable> void addTo(JsonElement jsonElement, Supplier<S> supplier) {
+        addTo(jsonElement, null, supplier);
+    }
+
+    public default void addTo(JsonElement jsonElement, String key, JSONSerializable jsonSerializable) {
+        addTo(jsonElement, key, (Supplier<JSONSerializable>) () -> jsonSerializable);
+    }
+
+    public default <S extends JSONSerializable, I extends Iterable<S>> void addAllTo(JsonElement jsonElement, String key, boolean skipNulls, Supplier<I> jsonSerializableSupplier) {
+        // Try to convert the iterable to a json array and add it to the addTo method
+        if (jsonSerializableSupplier == null) {
+            return;
+        }
+        Iterable<S> jsonSerializable;
+        try {
+            jsonSerializable = jsonSerializableSupplier.get();
+        } catch (final Throwable e) {
+            LOG.warn("Failed to retrieve JSON Serializable collection", e);
+            return;
+        }
+        Consumer<JsonElement> collectionAdder = jsonElement instanceof JsonObject jsonObject ?
+                (element) -> jsonObject.add(key, element) :
+                jsonElement instanceof JsonArray jsonArray ?
+                        jsonArray::add :
+                        null;
+        if (collectionAdder == null) {
+            return;
+        }
+        JsonArray jsonArray = new JsonArray();
+        for (JSONSerializable jsonSerializableElement : jsonSerializable) {
+            if (!skipNulls && jsonSerializableElement == null) {
+                // Exit early if we find a null element and skipNulls if false
+                return;
+            }
+            try {
+                var jsonElementValue = jsonSerializableElement != null ? jsonSerializableElement.toJSON() : null;
+                jsonArray.add(jsonElementValue);
+            } catch (final Throwable e) {
+                LOG.warn("Failed to convert JSONSerializable to JSONElement", e);
+            } finally {
+                if (!skipNulls) {
+                    return;
+                }
+            }
+        }
+        collectionAdder.accept(jsonArray);
+    }
+
+    public default <S extends JSONSerializable> void addAllTo(JsonElement jsonElement, String key, boolean skipNulls, Iterable<S> jsonSerializable) {
+        addAllTo(jsonElement, key, skipNulls, () -> jsonSerializable);
+    }
+
+    public default <S extends JSONSerializable, I extends Iterable<S>> void addAllTo(JsonElement jsonElement, boolean skipNulls, Supplier<I> jsonSerializableSupplier) {
+        addAllTo(jsonElement, null, skipNulls, jsonSerializableSupplier);
+    }
+
+    public default <S extends JSONSerializable> void addAllTo(JsonElement jsonElement, boolean skipNulls, Iterable<S> jsonSerializable) {
+        addAllTo(jsonElement, null, skipNulls, jsonSerializable);
+    }
 }
