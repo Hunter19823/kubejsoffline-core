f19ed8012e599aadcc1e4bbac58753a60294a20d
Hunter19823 <hspragg@asu.edu>
2025-08-02 15:28:08 -0700

    Made IndexedData comparable

--- src/main/java/pie/ilikepiefoo/kubejsoffline/core/api/datastructure/property/IndexedData.java
+++ src/main/java/pie/ilikepiefoo/kubejsoffline/core/api/datastructure/property/IndexedData.java
@@ -3,8 +3,13 @@ package pie.ilikepiefoo.kubejsoffline.core.api.datastructure.property;
 import pie.ilikepiefoo.kubejsoffline.core.api.JSONSerializable;
 import pie.ilikepiefoo.kubejsoffline.core.api.identifier.Index;
 
-public interface IndexedData<INDEX_TYPE extends Index> extends JSONSerializable {
+public interface IndexedData<INDEX_TYPE extends Index> extends JSONSerializable, Comparable<IndexedData<INDEX_TYPE>> {
     INDEX_TYPE getIndex();
 
     IndexedData<INDEX_TYPE> setIndex(INDEX_TYPE index);
+
+    @Override
+    default int compareTo(IndexedData<INDEX_TYPE> o) {
+        return getIndex().compareTo(o.getIndex());
+    }
 }
